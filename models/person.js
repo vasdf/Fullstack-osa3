4997fa65e5fdec3b7c519061fd4e097b8b4a6422
Valtteri <valtsu54@hotmail.com>
2018-02-09 19:25:25 +0200

    t3.20

--- models/person.js
+++ models/person.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URL 
 
 mongoose.connect(url)
